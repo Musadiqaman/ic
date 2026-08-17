1b72a3a8cc74c1791cdcbb7071951c2fd15f23a5
Musadiqaman <musadiqaman2006@gmail.com>
2026-08-17 14:45:28 +0500

    origin set

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ import { runAutoAttendance, generateMonthlyChallans } from "./src/controllers/st
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
